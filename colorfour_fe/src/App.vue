c96876d65b54f35d7d6a5f42b308061a545aa8da
hong <j520123456789@gmail.com>
2024-07-06 16:55:54 +0800

    remove bluma, add vue-index and images

--- colorfour_fe/src/App.vue
+++ colorfour_fe/src/App.vue
@@ -1,30 +1,85 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Header />
+    <main>
+      <router-view />
+    </main>
+    <Footer />
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import Header from "@/components/Header.vue";
+  import Footer from "@/components/Footer.vue";
 
-nav {
-  padding: 30px;
+  export default {
+    name: "App",
+    components: {
+      Header,
+      Footer,
+    },
+  };
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap");
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  :root {
+    --primary-bg-color: #ffffff;
+    --header-footer-bg-color: #e7d8c9;
+    --divider-color: #ccc;
+    --primary-text-color: #b2967d;
+    --button-bg-color: #e6b5ae;
+    --button-hover-bg-color: #d9a68c;
   }
-}
+
+  /* Reset and global styles */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Noto Sans TC", "Noto Sans", sans-serif;
+    font-weight: 500;
+  }
+
+  html,
+  body {
+    width: 100%;
+    min-height: 100%;
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--primary-bg-color);
+    padding-top: 70px; /* 確保不會被 header 蓋住 */
+    padding-bottom: 70px; /* 確保不會被 footer 蓋住 */
+  }
+
+  main {
+    width: 90%;
+    margin: 50px auto;
+    position: relative;
+    flex: 1;
+  }
+  
+  .btn-main {
+    background-color: var(--button-bg-color) !important;
+    border-color: var(--button-bg-color);
+    border-radius: 20px;
+    padding: 5px 10px;
+    text-decoration: none;
+    color: #fff;
+    display: inline-block;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+    transition: background-color 0.3s, border-color 0.3s;
+  }
+
+  .btn-main:hover {
+    background-color: var(--button-hover-bg-color); /* Change background color on hover */
+    border-color: var(--button-hover-bg-color);
+  }
 </style>

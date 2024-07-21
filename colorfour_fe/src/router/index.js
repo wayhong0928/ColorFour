017167de26686eb97368155cebf10d62091ee3aa
ling <ling920415@gmail.com>
2024-07-21 14:15:59 +0800

    google第三方登入(投降)

--- colorfour_fe/src/router/index.js
+++ colorfour_fe/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Login from "@/views/Login.vue";
 import Error404 from '@/views/Error404.vue';
+import Callback from '@/views/Callback.vue';
 
 const routes = [
   {
@@ -23,6 +24,11 @@ const routes = [
     path: '/:catchAll(.*)',
     name: 'Error404',
     component: Error404
+  },
+  {
+    path: "/callback",
+    name: "callback",
+    component: Callback
   }
 ];
 
